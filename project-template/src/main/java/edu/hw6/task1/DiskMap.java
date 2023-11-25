361c03158c3e83705c7ddd0c2a7f25aa862c4513
DucksInDrugs <ducksindrugs@gmail.com>
2023-11-26 01:37:01 +0400

    Complete hw7

--- project-template/src/main/java/edu/hw6/task1/DiskMap.java
+++ project-template/src/main/java/edu/hw6/task1/DiskMap.java
@@ -6,14 +6,20 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class DiskMap extends HashMap<String, String> implements Map<String, String> {
+public class DiskMap implements Map<String, String> {
     private final String fileName;
+    private final Map<String, String> map;
 
     public DiskMap(String fileName) {
         this.fileName = fileName;
+        map = new HashMap<>();
     }
 
     public boolean save() {
@@ -42,5 +48,85 @@ public class DiskMap extends HashMap<String, String> implements Map<String, Stri
             }
         }
         return false;
+    }
+
+    @Override
+    public int size() {
+        load();
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        load();
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        load();
+        return map.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        load();
+        return map.containsValue(value);
+    }
+
+    @Override
+    public String get(Object key) {
+        return map.get(key);
+    }
+
+    @Nullable
+    @Override
+    public String put(String key, String value) {
+        String previousValue = map.put(key, value);
+        save();
+        return previousValue;
+    }
+
+    @Override
+    public String remove(Object key) {
+        load();
+        String removedValue = map.remove(key);
+        save();
+        return removedValue;
+    }
+
+    @Override
+    public void putAll(@NotNull Map<? extends String, ? extends String> m) {
+        load();
+        map.putAll(m);
+        save();
+    }
+
+    @Override
+    public void clear() {
+        load();
+        map.clear();
+        save();
+    }
+
+    @NotNull
+    @Override
+    public Set<String> keySet() {
+        load();
+        return map.keySet();
+    }
+
+    @NotNull
+    @Override
+    public Collection<String> values() {
+        load();
+        return map.values();
+    }
+
+    @NotNull
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        load();
+        return map.entrySet();
     }
 }
